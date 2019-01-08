f017bee2761388c5159a46c76300a5b046b3ffb7
ChernovIA <Chernov.ilya.alekseevich@gmail.com>
2019-01-08 16:00:07 +0300

    set unique user field name

--- src/main/java/com/votes/myRestaurant/model/User.java
+++ src/main/java/com/votes/myRestaurant/model/User.java
@@ -18,7 +18,7 @@ public class User implements Model, UserDetails {
     @Column(name = "id")
     private long id;
 
-    @Column(name = "userName")
+    @Column(name = "userName", unique = true)
     private String name;
 
     @Column(name = "password")
